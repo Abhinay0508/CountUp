9757e87b49b39f949054f7084e283196d592d498
abhinaysahare05@gmail.com <61359913+Abhinay0508@users.noreply.github.com>
2023-02-07 16:06:56 +0530

    Updates

---
+++ precache-manifest.158b3f43578470e12034de4b8ca63c2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "001fc35c3ff27dbd322e",
+    "url": "/text-utilities/static/css/main.e3f7fd10.chunk.css"
+  },
+  {
+    "revision": "001fc35c3ff27dbd322e",
+    "url": "/text-utilities/static/js/main.001fc35c.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/text-utilities/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "e59fad3b99cf2a8b3099",
+    "url": "/text-utilities/static/js/2.e59fad3b.chunk.js"
+  },
+  {
+    "revision": "5a186a9158fb9a1ff9d9",
+    "url": "/text-utilities/static/js/runtime~main.5a186a91.js"
+  },
+  {
+    "revision": "2f5f0f1958277ad76de8786bdb4e4132",
+    "url": "/text-utilities/index.html"
+  }
+];
