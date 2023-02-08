df7059200a94ad2e59c713ac5278a84a6bf2677e
abhinaysahare05@gmail.com <abhinaysahare05@gmail.com>
2023-02-08 17:49:58 +0530

    <>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
     <nav class={`navbar bg-body-primary navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div class="container-fluid">
     <a class="navbar-brand" href="/">
-      <img src="/countup-high-resolution-logo-color-on-transparent-background.svg" alt='' width="70" height="42ß" class="d-inline-block align-text-middle"/>
+      <img src="/countuplogo.png" alt='' width="70" height="42" class="d-inline-block align-text-middle"/>
       <b>{props.title}</b>
     </a>
 
