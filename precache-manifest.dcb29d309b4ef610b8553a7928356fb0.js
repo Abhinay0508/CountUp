5ebb925739e25a35f472dfb6822f8c12b7d19062
abhinaysahare05@gmail.com <abhinaysahare05@gmail.com>
2023-02-09 11:19:41 +0530

    Updates

---
+++ precache-manifest.dcb29d309b4ef610b8553a7928356fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e836ebdc42f1a49463d4",
+    "url": "/CountUp/static/css/main.926eee1e.chunk.css"
+  },
+  {
+    "revision": "e836ebdc42f1a49463d4",
+    "url": "/CountUp/static/js/main.e836ebdc.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/CountUp/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/CountUp/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "bfe779073571de8c494c",
+    "url": "/CountUp/static/js/runtime~main.bfe77907.js"
+  },
+  {
+    "revision": "23f1648f5b6b09e421833ce5e26225c2",
+    "url": "/CountUp/index.html"
+  }
+];
